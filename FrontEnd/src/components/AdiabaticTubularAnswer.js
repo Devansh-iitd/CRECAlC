75e9af8adbf2a1635e073f4e0028cbf4df4e4653
Srajan Saxena <srajan.somerville@gmail.com>
2024-05-05 22:03:54 +0530

    Tubular done

--- FrontEnd/src/components/AdiabaticTubularAnswer.js
+++ FrontEnd/src/components/AdiabaticTubularAnswer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 const AdiabaticTubularAnswer = () => {
     const navigate = useNavigate();
     const handleButtonClick = () => {
@@ -10,10 +11,42 @@ const AdiabaticTubularAnswer = () => {
     <div className="container">
       <div className="content">
         <div className='row'>
-        <h1 className='headprocess2'>Adiabatic Tubular Reactor</h1>
+        <h1 className='headprocess3'>Adiabatic Tubular Reactor</h1>
         </div>
-        
-        <div className='row'><h2>Volume of Adiabatic Tubular Reactor is : </h2></div> 
+        <h2>    </h2>
+        <div className="row">
+          <div className="column hchart">
+          <LineChart width={300} height={300} data={[
+                { x: 1, volume: 100 },
+                { x: 2, volume: 150 },
+                { x: 3, volume: 125 },
+                // Add more data points as needed
+            ]}>
+                <XAxis dataKey="volume" tickSize={1} label={{ value: 'Volume', position: 'insideBottom', offset: -5 }}/>
+                <YAxis label={{ value: 'X(Conversion)', angle: -90, position: 'insideLeft' }}/>
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="x" stroke="#8884d8" />
+            </LineChart>
+          </div>
+          <div className="column hchart">
+          <LineChart width={300} height={300} data={[
+                { x: 1, volume: 100 },
+                { x: 2, volume: 150 },
+                { x: 3, volume: 125 },
+                // Add more data points as needed
+            ]}>
+                <XAxis dataKey="volume" tickSize={1} label={{ value: 'Volume', position: 'insideBottom', offset: -5 }}/>
+                <YAxis label={{ value: 'X(Conversion)', angle: -90, position: 'insideLeft' }}/>
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="x" stroke="#8884d8" />
+            </LineChart>
+          </div>
+        </div>
+        <h2>Volume of Adiabatic Tubular Reactor is : </h2>
         <div className='row'><button className="button2" onClick={handleButtonClick}>Home</button></div>
             
       </div>
