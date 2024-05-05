cc0f186db3bbf914a5b8009988474f660864a876
Srajan Saxena <srajan.somerville@gmail.com>
2024-05-05 17:05:22 +0530

    Frontend First commit

---
+++ FrontEnd/src/components/App.js
@@ -0,0 +1,12 @@
+import CenterDiv from "./CenterDiv";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='Heading'>CRE Calculator</h1>
+      <CenterDiv />
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd/src/components/CenterDiv.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CenterDiv = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <button className="button">Heat of Reaction</button>
+        <button className="button">Button 2</button>
+        <button className="button">Button 3</button>
+        <button className="button">Button 4</button>
+        <button className="button">Button 5</button>
+      </div>
+    </div>
+  );
+};
+
+export default CenterDiv;
