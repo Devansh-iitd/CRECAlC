8e95b917e1411d1f287649db594733ca9a8fb0c2
Devansh-iitd <gdevansh740@gmail.com>
2024-05-06 06:57:48 +0530

    Minnor Changes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const functions = require('./routes/functions');
+require('dotenv').config();
 
 
 app.use(express.json());
@@ -13,6 +14,6 @@ app.use(cors());
 app.use('/functions', functions);
 
 
-app.listen(5000, () => {
+app.listen(process.env.port || 5000, () => {
     console.log('Server is running on port 5000');
 });
